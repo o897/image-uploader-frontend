90d1e62ff89437d8254c6d20cf2ecf6532c440c1
Orapeleng Mathebula <71043883+o897@users.noreply.github.com>
2026-02-11 11:31:50 +0000

    fix anime bug

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Anime from "../pages/Anime";
 function Collection() {
-  const [photos, setPhotos] = useState();
+  const [photos, setPhotos] = useState([]);
 
   // render the collection that the user picked
   //  I already  set the set for the collection already
@@ -18,7 +18,7 @@ function Collection() {
       <div className="collections__photos">
         {photos ? (
           // fill this space with what the user already posted across all collections
-          <div>my photos</div>
+          <div><Anime/></div>
         ) : (
           <div className="colection__photos-show">
             <img
