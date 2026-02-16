aa57a46af92d17c4787b2fdbdcc881a3535bdbab
Orapeleng Mathebula <orapelengmathebula9@gmail.com>
2026-02-16 11:41:56 +0000

    update challenge intro comp

---
+++ src/components/ChallengeIntro.js
@@ -0,0 +1,41 @@
+import { useParams } from "react-router-dom";
+
+export default function ChallengeIntro() {
+  const challengesData = {
+    anime: {
+      title: "Anime Challenge",
+      description:
+        "Bring your favorite anime worlds to life. Upload fan art, dramatic scenes, cosplay, or stylized edits inspired by anime. Show your creativity and let your imagination lead.",
+    },
+
+    throwback: {
+      title: "Throwback Challenge",
+      description:
+        "Take us back in time. Share nostalgic moments, vintage-style photos, or memories that capture the feeling of the past. Upload something that tells a story from another era.",
+    },
+
+    scenery: {
+      title: "Scenery Challenge",
+      description:
+        "Capture the beauty around you. From sunsets and city skylines to mountains and quiet streets — upload your best scenery shots and let the view speak for itself.",
+    },
+
+    daily: {
+      title: "Weekly Highlights Challenge",
+      description:
+        "This week’s open theme. Upload your strongest and most creative image to stand out. No limits — just quality, originality, and impact.",
+    },
+  };
+  const { category } = useParams();
+
+  const data = challengesData[category];
+
+  if (!data) return null;
+
+  return (
+    <>
+      <div className="challenge__title">{data.title}</div>
+      <div className="challenge__title-sub">{data.description}</div>
+    </>
+  );
+}
--- src/pages/Challenge.js
+++ src/pages/Challenge.js
@@ -1,90 +1,85 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { FaRegThumbsUp } from "react-icons/fa";
-
+import ChallengeIntro from "../components/ChallengeIntro";
 
 const Challenge = () => {
-    const [photos, setPhotos] = useState([]);
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState();
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(
-                    "https://api.pexels.com/v1/curated?page=1&per_page=20",
-                );
+  const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
 
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.pexels.com/v1/curated?page=1&per_page=20"
+        );
 
-                const result = await response.json();
-                console.log("result photos", result);
-                setPhotos(result.photos); // ✅ photos only
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-        fetchData();
-    }, []);
+        const result = await response.json();
+        console.log("result photos", result);
+        setPhotos(result.photos); // ✅ photos only
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return (
-        <div className="challenge">
-            {/* Take it off the param  */}
-            <Navbar />
-            <div className="challenge__title">Anime</div>
-            <div className="challenge__title-sub">
-                Anime challenges created to inspire you to explore your favorite
-                characters, scenes, and art styles. Jump in, create, and share your
-                passion with others who love anime just as much as you do. No prizes —
-                just pure creativity and love for the culture.
-            </div>
-            {/* top 5 liked pictures */}
+    fetchData();
+  }, []);
 
-            {/* make a map it should rotate */}
-           
-            <div className="challenge-photos">
-                <div className="challenge-photos-item s">
-                    <img src="https://thenerddaily.com/wp-content/uploads/2018/08/Reasons-To-Watch-Anime.jpg?x26698" />
-                </div>
-                <div className="challenge-photos duo">
-                    <div className="challenge-photos-item m">
-                        <img src="https://comicbook.com/wp-content/uploads/sites/4/2025/01/One-Piece-Anime-Episodes-Hiatus-Watch.jpg" />
-                    </div>
-                    <div className="challenge-photos-item m">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsVElG1mHnr6oGMZa_FhCWF5iEXcKLbbF89A&s" />
-                    </div>
-                </div>
-                <div className="challenge-photos-item s">
-                    <img src="https://m.media-amazon.com/images/S/pv-target-images/9cef9a4a0cd4d6727d10a7d7ced004f09f3c9c03d50ee16feaf401b914703352._SX1080_FMjpg_.jpg" />
-                </div>
-                <div className="challenge-photos-item l">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHKNOZTz54_bsmQ7SWawYdiuxlky5pMwD-g&s" />
-                </div>
+  return (
+    <div className="challenge">
+      {/* Take it off the param  */}
+      <Navbar />
 
-                <div className="challenge-user">
-                    <h2>FirstName LastName</h2>
-                    <div>
-                        I’ve loved anime since childhood — the stories, the emotion, the
-                        powerful characters, and the worlds that felt bigger than reality.
-                        It wasn’t just entertainment; it was inspiration, imagination, and a
-                        place where I could escape and dream bigger.
-                    </div>
-                    <div className="challenge-user-stat">
-                        <div className="challenge-user-stat-item">
-                            <FaRegThumbsUp /> Like
-                        </div>
-                        <div className="challenge-user-stat-item">
-                            <button>Join the Challenge</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      {/* top 5 liked pictures */}
+      <ChallengeIntro />
+      {/* make a map it should rotate */}
+
+      <div className="challenge-photos">
+        <div className="challenge-photos-item s">
+          <img src="https://thenerddaily.com/wp-content/uploads/2018/08/Reasons-To-Watch-Anime.jpg?x26698" />
         </div>
-    );
+        <div className="challenge-photos duo">
+          <div className="challenge-photos-item m">
+            <img src="https://comicbook.com/wp-content/uploads/sites/4/2025/01/One-Piece-Anime-Episodes-Hiatus-Watch.jpg" />
+          </div>
+          <div className="challenge-photos-item m">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsVElG1mHnr6oGMZa_FhCWF5iEXcKLbbF89A&s" />
+          </div>
+        </div>
+        <div className="challenge-photos-item s">
+          <img src="https://m.media-amazon.com/images/S/pv-target-images/9cef9a4a0cd4d6727d10a7d7ced004f09f3c9c03d50ee16feaf401b914703352._SX1080_FMjpg_.jpg" />
+        </div>
+        <div className="challenge-photos-item l">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAHKNOZTz54_bsmQ7SWawYdiuxlky5pMwD-g&s" />
+        </div>
+
+        <div className="challenge-user">
+          <h2>FirstName LastName</h2>
+          <div>
+            I’ve loved anime since childhood — the stories, the emotion, the
+            powerful characters, and the worlds that felt bigger than reality.
+            It wasn’t just entertainment; it was inspiration, imagination, and a
+            place where I could escape and dream bigger.
+          </div>
+          <div className="challenge-user-stat">
+            <div className="challenge-user-stat-item">
+              <FaRegThumbsUp /> Like
+            </div>
+            <div className="challenge-user-stat-item">
+              <button>Join the Challenge</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Challenge;
