7a38f82e64a086ddd0c3688026792c7ee7582d5b
o897 <orapelengm239@gmail.com>
2026-04-24 17:33:40 +0200

    see user profile picture

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -44,7 +44,7 @@ const Profile = () => {
           <div className="profile_icon row">
             {/* Added a placeholder image and closed the tag */}
             <img
-              src="https://images.pexels.com/users/avatars/2158460592/orapeleng-mathebula-233.jpg?auto=compress&fit=crop&h=140&w=140&dpr=1"
+              src={user ? user?.user?.photo : "https://images.pexels.com/users/avatars/2158460592/orapeleng-mathebula-233.jpg?auto=compress&fit=crop&h=140&w=140&dpr=1"}
               alt="profile image"
             />
             <button>Change picture</button>
