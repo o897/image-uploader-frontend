fe4d83f108e090ddb9f02ebf842958f2fc4ac688
o897 <orapelengm239@gmail.com>
2026-05-03 15:49:09 +0200

    update register fetch

--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -1,42 +1,59 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const UpdateProfile = () => {
 
-  const [formData,setFormData] = useState({
-    fname : "",
-    lname : "",
-    about : "",
-    tiktokuname : "",
-    fcbkuname : "",
-    ytb : "",
-    privacy : "",
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    uname: "",
+    about: "",
+    tiktokuname: "",
+    fcbkuname: "",
+    ytb: "",
+    privacy: "",
   });
 
   const handleChange = (e) => {
     e.preventDefault();
 
     // from this target we are targetting this variables
-    const {name,value} = e.target;
+    const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-     [ name ] : value //where name match our input attach value
+      [name]: value //where name match our input attach value
     })
-  )}
+    )
+  }
 
-  const handleSubmit = async (e) => {
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
+  try {
     const response = await fetch("https://oraserver.online/auth/update", {
-      method : "PUT",
-      credentials : 'include'
-    })
+      method: "PUT",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" }, // ← you had "COntent-Type" typo
+      body: JSON.stringify(formData)
+    });
 
     if (!response.ok) {
-        console.log("something wrong with your data")
+      const err = await response.json();
+      console.log("server error:", err);
+      return;
     }
 
+    navigate('/');
+
+  } catch (error) {
+    console.log("network error:", error);
   }
+}
+
 
   return (
     <>
@@ -52,7 +69,7 @@ const UpdateProfile = () => {
               <div className="profile_icon updt-img">
                 {/* Added a placeholder image and closed the tag */}
                 <img
-                  src="https://images.pexels.com/users/avatars/2158460592/orapeleng-mathebula-233.jpg?auto=compress&fit=crop&h=140&w=140&dpr=1"
+                  src='https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3408.jpg?w=360'
                   alt="profile image"
                 />
                 <button className='submit__img-btn'>Change</button>
@@ -60,53 +77,53 @@ const UpdateProfile = () => {
               <div className='prof-row'>
                 <div className='prof-col'>
                   <label>First Name</label>
-                  <input name='fname' className='prof-input' onChange={setFormData} placeholder='Enter your firstname'/>
+                  <input name='fname' className='prof-input' onChange={setFormData} placeholder='Enter your firstname' />
                 </div>
                 <div className='prof-col'>
                   <label>Last name</label>
-                  <input name='lname' className="prof-input" placeholder='Enter your lastname'/>
+                  <input name='lname' className="prof-input" onChange={setFormData} placeholder='Enter your lastname' />
                 </div>
               </div>
-            
+
               <div className='prof-col'>
                 <label>Username</label>
-                <input name='uname' className='prof-input' placeholder='Enter your username'/>
+                <input name='uname' className='prof-input' onChange={setFormData} placeholder='Enter your username' />
               </div>
               <div className='prof-col'>
                 <label>About</label>
-                <textarea name="about" rows="4" cols="50" placeholder='Fun fact about you...'>
+                <textarea name="about" rows="4" cols="50" onChange={setFormData} placeholder='Fun fact about you...'>
                 </textarea>
               </div>
-               <div className='prof-row'>
-                
+              <div className='prof-row'>
+
                 <div className='prof-col'>
                   <label>Youtube</label>
-                  <input name="yname" className="prof-input" placeholder='Enter your Youtube username'/>
+                  <input name="yname" className="prof-input" onChange={setFormData} placeholder='Enter your Youtube username' />
                 </div>
-                 <div className='prof-col'>
+                <div className='prof-col'>
                   <label>Facebook</label>
-                  <input name="fcbkuname" className='prof-input' placeholder='facebook username'/>
+                  <input name="fcbkuname" className='prof-input' onChange={setFormData} placeholder='facebook username' />
                 </div>
               </div>
-               <div className='prof-row'>
-               
-                <div className='prof-col'>
+              <div className='prof-row'>
+
+                {/* <div className='prof-col'>
                   <label>Privacy</label>
                   <select className='prof-input select'>
-                      <option value="public">Public</option>
-                      <option value="private">Private</option>
+                    <option value="public">Public</option>
+                    <option value="private">Private</option>
                   </select>
-                </div>
+                </div> */}
               </div>
-              
-              <button className='submit__img-btn'>Submit</button>
+
+              <button className='submit__img-btn' onClick={handleSubmit} >Submit</button>
 
 
             </div>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
 
 
